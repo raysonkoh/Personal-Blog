cc6a4e88facbf6f2bdd106ca9b81cda3935c79f0
Rayson Koh <koh_rayson@yahoo.com.sg>
2019-08-12 23:07:44 +0800

    setup css loaders and sass watchers

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
@@ -14,6 +15,17 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    }
 }
