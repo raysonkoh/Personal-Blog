e5936404389bcbd1ee624a6930f3c2b30de96fe3
Rayson Koh <koh_rayson@yahoo.com.sg>
2019-08-14 00:04:10 +0800

    added 3 main-content and footer

--- src/homepage/js/index.js
+++ src/homepage/js/index.js
@@ -1,5 +1,6 @@
-import DOM_strings from './DOM';
 import '../../shared.css';
+import '../style.css';
+import DOM_strings from '../../DOM';
 
 DOM_strings.dropdown__selector.addEventListener('click', e => {
     DOM_strings.dropdown__menu.classList.toggle('open');
