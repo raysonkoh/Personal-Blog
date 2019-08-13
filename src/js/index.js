ac3b4ccc43dff7ac582d8b39aa470019aba43912
Rayson Koh <koh_rayson@yahoo.com.sg>
2019-08-13 11:43:54 +0800

    settled fonts

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,5 @@ import DOM_strings from './DOM';
 import '../style.css';
 
 DOM_strings.dropdown__selector.addEventListener('click', e => {
-    DOM_strings.dropdown__menu.style.display = 'block';
+    DOM_strings.dropdown__menu.classList.toggle('open');
 })
